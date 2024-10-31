50c9bb1ec4aee0678391fa8544cb1a2a578418cf
moroz <test>
2024-10-31 19:44:27 +0100

    1

--- SecondProject/Program.cs
+++ SecondProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Second Project!");
+            Console.WriteLine("Hello, Second Project1!");
         }
     }
 }
